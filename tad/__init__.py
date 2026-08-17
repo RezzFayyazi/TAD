e1e6fef2d2ffc622474839fcac7ce8b88150e29e
Reza Fayyazi <rezafayyazi@Rez-262.local>
2026-08-16 21:17:57 -0400

    Update

--- tad/__init__.py
+++ tad/__init__.py
@@ -1,4 +1,6 @@
 
+# Licensed under the GNU General Public License v3.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at https://www.gnu.org/licenses/gpl-3.0.en.html
+
 import importlib
 import os
 
--- tad/__main__.py
+++ tad/__main__.py
@@ -1,3 +1,5 @@
+# Licensed under the GNU General Public License v3.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at https://www.gnu.org/licenses/gpl-3.0.en.html
+
 from tad.cli import main
 
 if __name__ == "__main__":
